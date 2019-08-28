e757f23ec8f0bf0e9c8346644e640dab25968975
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-28 12:48:52 -0700

    created challenges table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190828192422) do
+ActiveRecord::Schema.define(version: 20190828194549) do
+
+  create_table "challenges", force: :cascade do |t|
+    t.string  "title"
+    t.string  "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
