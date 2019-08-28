ec4ac392994850c483b886356d01d25fb52bd687
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-28 12:26:46 -0700

    created users migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
         erb :'/users/signup'
     end
 
+    post '/signup' do 
+        if params[:username] != "" && params[:email] != "" && params[:passwprd] != ""
+            @user = User.new(params)
+            @user.save
+            session[:user_id] = @user.id 
+            redirect '/challenges'
+        else
+            redirect '/signup'
+        end
+    end
+
+
 
 
 end
