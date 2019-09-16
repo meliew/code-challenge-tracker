1aea5636118eab3dd1f945da4ff1d8f882e81d1a
Melissa Williams <melissa@brandmeetscopy.com>
2019-09-16 15:02:27 -0700

    removed unused methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,6 @@ class User < ActiveRecord::Base
     validates :username, uniqueness: true 
     has_secure_password
     has_many :challenges
-    
-    def slug 
-        username.downcase.gsub(" ", "-")
-    end
-
-    def self.find_by_slug(slug)
-        User.all.find do |user|
-            user.slug == slug 
-        end 
-    end
 
 
 end 
